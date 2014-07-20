08e1efc06252c5401254868837f835b78033eb68
Nigel Blackmore <nigel@blackmore.de>
2014-07-20 06:43:52 +0200

    First init

---
+++ config/enviroment.rb
@@ -0,0 +1,8 @@
+
+ROOT = File.expand_path('../../', __FILE__)
+
+# set this ti the path of the folder you wish to watch
+SOURCE_PATH = "/Users/nigel/Documents/Code2/media/mediaserver/advantage_2_midi"
+
+# set this to the path where to place the sources once finished
+PROCESSED_PATH = "#{SOURCE_PATH}/processed"
